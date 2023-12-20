fda203968160a5a7ba36e3ef80e170f49d1c302a
Dimas Bagus Wahyu Sudrajat <47965622+dimas150798@users.noreply.github.com>
2023-12-20 14:33:06 +0700

    m

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '49d2960b007b29b9a2f3b85051c8e0f9d2cb1716',
+        'reference' => '5e3ee3d07e3f7dc7fa338047d41fca920ff791ef',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -382,7 +382,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '49d2960b007b29b9a2f3b85051c8e0f9d2cb1716',
+            'reference' => '5e3ee3d07e3f7dc7fa338047d41fca920ff791ef',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -757,8 +757,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '3.0.0',
-                1 => '1.0|2.0|3.0',
+                0 => '1.0|2.0|3.0',
+                1 => '3.0.0',
             ),
         ),
         'psr/simple-cache' => array(
