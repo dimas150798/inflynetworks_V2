c865a79542b01336ff3de9ea2a3563ed8ba5c3ca
Dimas Bagus Wahyu Sudrajat <47965622+dimas150798@users.noreply.github.com>
2024-01-15 16:20:39 +0700

    memperbarui jarak enjoy

--- resources/views/V_Home.blade.php
+++ resources/views/V_Home.blade.php
@@ -84,7 +84,7 @@
             </div>
         </div>
 
-        <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+        {{-- <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
             viewBox="0 24 150 28 " preserveAspectRatio="none">
             <defs>
                 <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z">
@@ -98,19 +98,18 @@
             <g class="wave3">
                 <use xlink:href="#wave-path" x="50" y="9" fill="#fff">
             </g>
-        </svg>
+        </svg> --}}
 
     </section><!-- End Hero -->
 
     <main id="main">
-
 
         <section id="about" class="about">
             <div class="container-fluid">
 
                 <div class="row">
 
-                    <div class="col-xl-5 col-lg-6 d-flex justify-content-center align-items-stretch mt-4"
+                    <div class="col-xl-5 col-lg-6 d-flex justify-content-center align-items-stretch"
                         data-aos="fade-right">
                         <div class="img-enjoy">
                             <img src="{{ asset('assets/img/gallery/bg-enjoy.png') }}" alt="">
@@ -246,7 +245,8 @@
         </section><!-- End Features Section -->
 
 
-    </main><!-- End #main -->
+    </main>
+    <!-- End #main -->
 
     <!-- ======= Footer ======= -->
     <footer id="footer">
